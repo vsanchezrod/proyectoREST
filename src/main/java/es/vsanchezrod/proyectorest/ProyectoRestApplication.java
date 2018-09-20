831f3f649328d96b32bfa6b16a15d023d3fc1b6d
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-20 12:37:23 +0200

    Proyecto maven configurado

---
+++ src/main/java/es/vsanchezrod/proyectorest/ProyectoRestApplication.java
@@ -0,0 +1,12 @@
+package es.vsanchezrod.proyectorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRestApplication.class, args);
+	}
+}
